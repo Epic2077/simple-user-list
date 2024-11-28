4eae96e3a315c5f4091650fc8e3e98f70ebae080
Ashkan <the.epic.2077@gmail.com>
2024-11-28 15:50:04 +0330

    Feat:
    UserTable

--- src/App.tsx
+++ src/App.tsx
@@ -1,2 +1,32 @@
-function App() {}
+import { useState } from "react";
+import UsersTable, { User } from "./components/UsersTable";
+import UserForm from "./components/UserForm";
+import { initialUsers } from "./userData"; // Update the path as needed
+
+function App() {
+  const [users, setUsers] = useState<User[]>(initialUsers);
+
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    const formData = new FormData(event.currentTarget);
+    const newUser = Object.fromEntries(formData.entries()) as unknown as User;
+    const max = Math.max(...users.map((user) => user.id));
+    setUsers([...users, { ...newUser, id: max + 1 }]);
+    event.currentTarget.reset();
+  };
+
+  const handleDeleteUser = (id: number) => {
+    setUsers(users.filter((user) => user.id !== id));
+  };
+
+  return (
+    <div className="container mx-auto p-2">
+      <div className="flex items-start gap-3">
+        <UsersTable users={users} onDeleteUser={handleDeleteUser} />
+        <UserForm onSubmit={handleSubmit} />
+      </div>
+    </div>
+  );
+}
+
 export default App;
---
+++ src/components/userData.tsx
@@ -0,0 +1,8 @@
+import { User } from "./components/UsersTable"; // Update the path as needed
+
+export const initialUsers: User[] = [
+  { id: 1, name: "ali", lastName: "komijani", email: "ali@gmail.com" },
+  { id: 2, name: "ali", lastName: "komijani", email: "ali@gmail.com" },
+  { id: 5, name: "ali", lastName: "komijani", email: "ali@gmail.com" },
+  { id: 3, name: "ali", lastName: "komijani", email: "ali@gmail.com" },
+];
